c8c6866b382d34f5eb1bbfe032aa0824b8a3ec8b
Christopher <42280549+christophercsilva@users.noreply.github.com>
2020-04-22 23:16:40 -0300

    initial commit
    Introduction learns

---
+++ introduction.js
@@ -0,0 +1,34 @@
+
+// Exibe o ano atual
+console.log(2020);
+
+// Comentário de uma linha
+
+/* Comentário 
+   de múltiplas
+   linhas
+*/
+
+// Tipo de dados
+let number = 0; // número
+let string = "string"; // string
+let boolean = true; // booleano
+let nulll = null; // nulo
+let undefinedd = undefined; // indefinido
+let symbol = "i don't know yet";
+let object = {string, boolean };
+
+// Operadores aritméticos
+console.log(1 + 2); // adição
+console.log(3 - 4); // subtração
+console.log(5 * 6); // multiplicação
+console.log(7 / 8); // divisão
+console.log(9 % 10); // resto da divisão
+
+// Concatenação de string
+console.log('Um' + ', ' + 'dois');
+
+// Propriedades
+console.log("Christopher".length) // Exemplo de uma propriedade
+
+// Métodos
