e0296dccd9d36ec4219970caa0192b5bf86911db
Christopher <42280549+christophercsilva@users.noreply.github.com>
2020-04-22 23:54:28 -0300

    add methods

--- introduction.js
+++ introduction.js
@@ -10,13 +10,13 @@ console.log(2020);
 */
 
 // Tipo de dados
-let number = 0; // número
+let numerico = 0; // número
 let string = "string"; // string
 let boolean = true; // booleano
-let nulll = null; // nulo
-let undefinedd = undefined; // indefinido
-let symbol = "i don't know yet";
-let object = {string, boolean };
+let nulo = null; // nulo
+let indefinido = undefined; // indefinido
+let simbolo = "i don't know yet";
+let objeto = {string, boolean };
 
 // Operadores aritméticos
 console.log(1 + 2); // adição
@@ -32,3 +32,6 @@ console.log('Um' + ', ' + 'dois');
 console.log("Christopher".length) // Exemplo de uma propriedade
 
 // Métodos
+// Classes podem ter métodos, métodos são ações, é algo especifico que uma classe faz, podem ter um retorno ou não
+console.log('Gerando número aleatório de 0 a 100: ', Math.random * 100);
+console.log('Tudo isso ficará em maiúsculo: '.toUpperCase());
