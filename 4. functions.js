b5fab033b390e650bfa52323e6bbff279598ee95
Christopher <42280549+christophercsilva@users.noreply.github.com>
2020-04-25 20:28:54 -0300

    initial commit functions

---
+++ 4. functions.js
@@ -0,0 +1,8 @@
+/* Imagine que queremos calcular a área de um retângulo, precisamos só de duas variáveis para calcular
+    sendo elas largura e altura, basta declararmos-as e imprimir o resultado, mas imagine que precisemos
+    calcular a área de 10 retângulos, fica inviàvel declarar 20 variáveis! Para isso existem as funções,
+    elas nos permitem a reutilização de código
+*/
+function areaRet(largura, altura){
+    return largura * altura;
+}
