0afabbf063bca6f963a44adca8436704e115f3eb
Christopher <42280549+christophercsilva@users.noreply.github.com>
2020-04-27 16:40:02 -0300

    Projects of introduction

---
+++ Projects/1. Introduction/2. dog years.js
@@ -0,0 +1,20 @@
+// minha idade em 2020
+let myAge = 22;
+
+// earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+// Calcula a idade de cachorro de acordo com os últimos anos
+laterYears *= 4;
+
+console.log(earlyYears, laterYears);
+
+// Minha idade em cachorro
+myAgeInDogYears = earlyYears + laterYears;
+
+// Meu nome em lowercase
+const myName = 'Christopher'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
